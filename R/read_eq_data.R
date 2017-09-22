347a8648be37f3d74699609e1cac1ce582f9c940
Coursera Learner 42 <coursera.learner42@gmail.com>
2017-09-22 07:48:00 +0000

    Separate eq_location_clean. Remove the last NOTE.

--- R/read_eq_data.R
+++ R/read_eq_data.R
@@ -4,8 +4,14 @@ library(magrittr)
 #'
 #' @param raw_data_fn A path to the raw data filename, default to the signif.txt delivered by the
 #'     package
-#'
 #' @return A clean data.frame
+#'
+#' @importFrom readr read_delim
+#'
+#' @examples \dontrun{
+#'   clean_data <- read_eq_clean_data()
+#' }
+
 #' @export
 read_eq_clean_data <-function(raw_data_fn = file.path(system.file("extdata",
                                                                   package="CourseraRcapstone"),
@@ -17,16 +23,36 @@ read_eq_clean_data <-function(raw_data_fn = file.path(system.file("extdata",
 #' Takes raw NOAA data frame and returns a clean data frame
 #'
 #' @param df The uncleaned data.frame
-#'
 #' @return A clean data.frame
+#'
+#' @importFrom dplyr %>% mutate
+#' @importFrom tidyr unite_ extract_
+#' @importFrom stringr str_to_title
+#'
+#' @examples \dontrun{
+#'   raw_data <- readr::read_delim(raw_data_fn, "\t"))
+#'   clean_data <- eq_clean_data(raw_data)
+#' }
 #' @export
 eq_clean_data <-function(df) {
     df %>%
-        tidyr::unite(date, rlang::sym("YEAR"), rlang::sym("MONTH"), rlang::sym("DAY")) %>%
-        dplyr::mutate(date = lubridate::ymd(date)) %>%
-        dplyr::mutate(LONGITUDE = as.numeric(rlang::sym("LONGITUDE")),
-                      LATITUDE = as.numeric(rlang::sym("LATITUDE"))) %>%
-        tidyr::extract(rlang::sym("LOCATION_NAME"), c("LOCATION_NAME_COUNTRY", "LOCATION_NAME"),
-                       regex = "(\\w+):\\s+(\\w.+)") %>%
-        dplyr::mutate(LOCATION_NAME = stringr::str_to_title(rlang::sym("LOCATION_NAME")))
+        tidyr::unite_("DATE", c("YEAR", "MONTH", "DAY")) %>%
+        dplyr::mutate_(DATE = ~lubridate::ymd(DATE)) %>%
+        dplyr::mutate_(LONGITUDE = ~as.numeric(LONGITUDE),
+                       LATITUDE = ~as.numeric(LATITUDE)) %>%
+        eq_location_clean()
+}
+
+#' Clean up the LOCATION_NAME column
+#' @param df The uncleaned data.frame
+#' @return A clean data.frame
+#'
+#' @importFrom tidyr unite_ extract_
+#' @importFrom dplyr mutate_
+eq_location_clean <- function(df) {
+    df %>%
+        tidyr::extract_("LOCATION_NAME", c("LOCATION_NAME_COUNTRY", "LOCATION_NAME"),
+                        regex = "(\\w+):\\s+(\\w.+)") %>%
+        dplyr::mutate_(LOCATION_NAME = ~stringr::str_to_title(LOCATION_NAME))
+
 }
